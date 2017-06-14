874d7dc7035f770999a2b14ae2541b5666950954
jakabadambalazs <jakabadambalazs@gmail.com>
2017-06-14 19:11:37 +0200

    releasing 0.0.3

---
+++ bin/create-release.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+APKDIR="platforms/android/build/outputs/apk"
+KEYSTORE="/home/jack/Documents/Keystore/mekit-release-key.keystore"
+ZIPALIGNBIN="/home/jack/Android/Sdk/build-tools/25.0.3/zipalign"
+
+if [ ! -d "${APKDIR}" ]; then
+  echo "NO APK DIR! Run this script from project root!"
+  exit 1
+fi
+
+
+if [ -f "${APKDIR}/android-release-unsigned.apk" ]; then
+  echo "Deleting stale unsigned release version..."
+  rm -rf "${APKDIR}/android-release-unsigned.apk"
+fi
+
+if [ -f "${APKDIR}/android-release-signed.apk" ]; then
+  echo "Deleting stale signed release version..."
+  rm -rf "${APKDIR}/android-release-signed.apk"
+fi
+
+
+
+echo "Creating unsigned release version..."
+ionic cordova build --release android
+
+if [ ! -f "${APKDIR}/android-release-unsigned.apk" ]; then
+  echo "NO APK - BUILD FAILED!"
+  exit 2
+fi
+
+echo "Signing release version..."
+jarsigner -sigalg SHA1withRSA -digestalg SHA1 -keystore ${KEYSTORE} -storepass mekitscs "${APKDIR}/android-release-unsigned.apk" mekit_release
+
+
+echo "Creating zipaligned signed version..."
+${ZIPALIGNBIN} -v 4 "${APKDIR}/android-release-unsigned.apk" "${APKDIR}/android-release-signed.apk"
+
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -19,7 +19,7 @@ export class UserService
   private autologin: any = {
     autologin: false,
     autologin_user: 'admin',
-    autologin_pwd: 'admin',
+    autologin_pwd: '',
   };
 
   constructor(private configurationService: ConfigurationService
